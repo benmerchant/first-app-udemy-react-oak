89d57fe331eeea8514fdc8c1d48bea5a39a6b496
Ben Merchant <benjaminmerchant@gmail.com>
2022-09-13 18:58:01 -0400

    closes #4 class based components

--- src/index.js
+++ src/index.js
@@ -1,41 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SingleComment from './SingleComment';
-import UserCard from './UserCard';
-import tatiana from './images/tatiana.jpg';
-import emily from './images/emily.jpg';
-import krysten from './images/krysten.jpg';
-
-const App = () =>{
-  return (
-    <div className='ui comments'>
-      <UserCard>
-        <SingleComment
-          name='emily'
-          text='go sox'
-          date='Today at 00:00' 
-          pfp={emily}
-        />
-      </UserCard>
-      <UserCard>
-        <SingleComment
-          name='tatiana'
-          text='dont make me angry'
-          date='Thursday at 03:00' 
-          pfp={tatiana}
-        />
-      </UserCard>
-      <UserCard>
-        <SingleComment
-          name='krysten'
-          text='secure the bag'
-          date='Wednesday at 21:00' 
-          pfp={krysten}
-        /> 
-      </UserCard>
-      
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    return(
+      <div>
+        You are in the Northern Hemisphere
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
