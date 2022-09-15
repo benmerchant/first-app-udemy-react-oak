f5a021bab9bf27780af833b2cb8f5e9bf14791b4
Ben Merchant <benjaminmerchant@gmail.com>
2022-09-15 10:13:26 -0400

    clear out files for new app

--- src/index.js
+++ src/index.js
@@ -1,34 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HemisphereDisplay from './HemisphereDisplay';
-
 class App extends React.Component {
-  state = { latitude: null, errorMessage: ''}
+  state = { }
 
   componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({latitude: position.coords.latitude})
-      },
-      (error) => {
-        this.setState({errorMessage: error.message})
-      }
-    );
   }
 
   // componentDidUpdate() {
     
   // }
   render() {
-    if(this.state.errorMessage && !this.state.latitude) {
-      return <div>{this.state.errorMessage}</div>
-    }
-    if(!this.state.errorMessage && this.state.latitude) {
-      return <div><HemisphereDisplay latitude={this.state.latitude} /></div>
-    }
-    else {
-      return <div>Loading...</div>
-    }
+    return(
+      <div></div>
+    )
   }
 }
 
